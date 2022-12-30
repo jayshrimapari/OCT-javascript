2d697cf195d5868de2c1a4e351a587d041c27c09
jayshrimapari <jayshrimapari5555@gmail.com>
2022-12-30 14:45:02 +0530

    getelementbyid

--- getElementById.js
+++ getElementById.js
@@ -4,3 +4,15 @@ function changeColor(xyz){
     element.style.color = xyz;
  
  }
+//  function addition(a,b):number{
+//     let a = 10;
+//     let b = 20;
+//     return a+b;
+//  }
+// const res = (a,b) => {
+//     return a+b;
+// }
+const res = (a,b) => a+b;
+console.log(res(10,20));
+// console.log(addition(10,20));
+// console.log('res',res);
