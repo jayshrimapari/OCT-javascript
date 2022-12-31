5c1b12a63cff27ec0a11267a7d1826e96f32ccc7
jayshrimapari <jayshrimapari5555@gmail.com>
2022-12-31 12:51:35 +0530

    added events file

---
+++ events.js
@@ -0,0 +1,18 @@
+function eventsFun(data){
+    console.log(data);
+    console.log('events',data.value);
+
+}
+function blureventsFun(){
+    console.log();
+}
+function onchangeeventhandlerFun(event){
+    console.log(event.checked);
+
+}
+function focusFunction(ele){
+    ele.style.backgroundColor = 'orange';
+}
+function blurFunction(params) {
+    params.style.backgroundColor = 'yellow'
+}
