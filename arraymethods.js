ac009e28af04d8ee118aaf852bcf428b9aa12a5c
jayshrimapari <jayshrimapari5555@gmail.com>
2023-01-16 22:50:08 +0530

    regex files

--- arraymethods.js
+++ arraymethods.js
@@ -3,8 +3,14 @@ emp = [
     { firstName:'jayu1',lastName:'bhokare1', age: 25 },
     { firstName:'jayu2',lastName:'bhokare2', age: 26 }
 ]
-let data = emp.filter((element)=>{
-    return element.age == 50;
-})
-console.log(data);
+// let data = emp.filter((element)=>{
+//     return element.age == 25;
+// })
+// console.log(data);
 
+console.log(emp[1].firstName);
+let newData =[];
+emp.forEach(element => {
+    newData.push(element.firstName)
+});
+console.log(newData);
---
+++ regEx.js
@@ -0,0 +1,13 @@
+var text = "i live in mumbai,mumbai,mumbai,MUMbai,m,mbai,mmm,mm"
+console.log(text.match(/mu+/g));
+var text = "i live in mumbai,mumbai,mumbai,MUMbai,m,mbai,mmm,mm"
+console.log(text.match(/mm*/g));
+
+
+let num = 5.123456;
+//para : length
+console.log(num.toPrecision());
+console.log(num.toPrecision(5));
+console.log(num.toPrecision(2));
+console.log(num.toPrecision(1));
+
